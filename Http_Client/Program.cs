5245e55c7a3ef22cdb1c9f02a39f1d9866467cec
novruzov_t <novruzov_t@STHQ0123-01>
2023-03-18 12:39:56 +0400

    last

--- Http_Client/Program.cs
+++ Http_Client/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using var client = new HttpClient();
+
+//var responseMessage = await client.GetAsync(@"http://localhost:27001/");
+var responseMessage = await client.GetAsync(@"http://localhost:27001/?id=1&name=Leyla");
+
+var content = await responseMessage.Content.ReadAsStringAsync();
+
+Console.WriteLine(content);
--- Http_Listener/Program.cs
+++ Http_Listener/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net;
+
+
+using var listener = new HttpListener();
+
+listener.Prefixes.Add(@"http://localhost:27001/");
+listener.Prefixes.Add(@"http://localhost:27002/");
+
+listener.Start();
+
+Console.WriteLine("Listener...");
+
+while (true)
+{
+    var context = await listener.GetContextAsync();
+
+    var request = context.Request;
+    var response = context.Response;
+
+    // for (int i = 0; i < request.QueryString.Count; i++)
+    // {
+    //     var value = request.QueryString[i];
+    //     var key = request.QueryString.Keys[i];
+    //     Console.WriteLine($"{key} = {value}");
+    // }
+
+
+    response.ContentType = "application/json";
+    //response.Headers.Add("Content-Type", "text/plain");
+    response.Headers.Add("Content-Type", "text/html");
+    response.StatusCode = 200;
+
+
+    var writer = new StreamWriter(response.OutputStream);
+    await writer.WriteLineAsync("<h1 style='color:blue;'>Hello World</h1>");
+
+    writer.Close();    
+}
