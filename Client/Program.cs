3df1c6bc722d503c5f92ccabb1da26ed9476b40b
novruzov_t <novruzov_t@STHQ0123-01>
2023-03-18 11:02:57 +0400

    Add project files.

---
+++ Client/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using System.Net;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// HttpWebRequest request = HttpWebRequest.CreateHttp("https://www.google.com/");
+// var response  = request.GetResponse();
+// 
+// using var responseStream = response.GetResponseStream();
+// using var reader = new StreamReader(responseStream);
+// 
+// Console.WriteLine(reader.ReadToEnd());
+
+
+
+
+
+// var client = new WebClient();
+// var responseString = client.DownloadString("https://www.google.com/");
+// Console.WriteLine(responseString);
